b7e0a8457b3714b85add23c82a2a73a5646ac77b
des-tinyDM <destinyleaross@gmail.com>
2019-04-18 20:14:15 -0400

    shop by cat with images

--- src/components/ShopByCategory.js
+++ src/components/ShopByCategory.js
@@ -9,13 +9,29 @@ class ShopByCategory extends Component {
     return (
       <div className="ShopByCat__Container">
         <h2 className="ShopByCat__Headline">Shop by Category</h2>
-        <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--herbsandoils" />
-        <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--candles" />
-        <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--crystalsrunes" />
-        <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--tarot" />
-        <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--books" />
-        <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--altar" />
-        <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--misc" />
+        <div className="ShopByCat__CatContainer">
+          <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--herbsandoils">
+            <h3 className="CatLink">Herbs and Essential Oils</h3>
+          </div>
+          <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--candles">
+            <h3 className="CatLink">Candles</h3>
+          </div>
+          <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--crystalsrunes">
+            <h3 className="CatLink">Crystals and Runes</h3>
+          </div>
+          <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--tarot">
+            <h3 className="CatLink">Tarot</h3>
+          </div>
+          <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--books">
+            <h3 className="CatLink">Books</h3>
+          </div>
+          <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--altar">
+            <h3 className="CatLink">Altars and Tools</h3>
+          </div>
+          <div className="ShopByCat__CategoryThumbnail ShopByCat__CategoryThumbnail--misc">
+            <h3 className="CatLink">Miscellaneous</h3>
+          </div>
+        </div>
       </div>
     );
   }
